c80ad887cd8def2e62f263943720dcceadd134e2
FormClippy <armwong5136@gmail.com>
2024-12-28 16:44:41 +0700

    1

--- Product/product.js
+++ Product/product.js
@@ -3,27 +3,49 @@ let slideIndex = 1;
 function showSlides(n) {
 
     const slides = document.getElementsByClassName('banner-wrapper')
-
+    
+    
     if (n > slides.length) {
         slideIndex = 1;
     }
-
+    
     if (n < 1) {
         slideIndex = slides.length;
     }
-
     for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
 
     slides[slideIndex - 1].style.display = "flex";
+
 }
 
-showSlides();
+showSlides(slideIndex);
 
 function plusSlides(n) {
     showSlides(slideIndex += n);
 }
 
-let modal = document.getElementById('myModal');
-let modalImg = document.getElementById("img01");
+
+
+
+let productDesc = document.getElementsByClassName('all-grid-item-desc');
+let productBtn = document.getElementsByClassName('all-grid-item-btn');
+let productClose = document.getElementsByClassName('all-grid-item-desc-x');
+
+for (let i = 0; i < productBtn.length; i++) {
+    productBtn[i].addEventListener('click', function () {
+        productDesc[i].style.display = 'block';
+    
+        productDesc[i].classList.add('popup'); // เพิ่ม class "show"
+    });
+}
+
+for (let i = 0; i < productClose.length; i++) {
+    productClose[i].addEventListener('click', function () {
+        productDesc[i].style.display = 'none';
+    });
+}
+
+
+
