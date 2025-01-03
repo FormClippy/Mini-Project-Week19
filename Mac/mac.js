d491e22a2679e8bd05bd9ad6d932788c8a7215e8
FormClippy <armwong5136@gmail.com>
2025-01-03 16:19:13 +0700

    1

---
+++ Mac/mac.js
@@ -0,0 +1,64 @@
+let slideIndex = 1;
+
+function showSlides(n) {
+
+    const slides = document.getElementsByClassName('banner-wrapper')
+    
+    
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex - 1].style.display = "flex";
+
+}
+
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+
+
+
+let productDesc = document.getElementsByClassName('all-grid-item-desc');
+let productBtn = document.getElementsByClassName('all-grid-item-btn');
+let productClose = document.getElementsByClassName('all-grid-item-desc-x');
+
+for (let i = 0; i < productBtn.length; i++) {
+    productBtn[i].addEventListener('click', function () {
+        productDesc[i].style.display = 'block';
+    
+        productDesc[i].classList.add('popup'); // เพิ่ม class "show"
+    });
+}
+
+for (let i = 0; i < productClose.length; i++) {
+    productClose[i].addEventListener('click', function () {
+        productDesc[i].style.display = 'none';
+    });
+}
+
+function searchProducts() {
+    const input = document.getElementById("searchInput").value.toLowerCase();
+    const products = document.getElementsByClassName("all-grid-item");
+
+    Array.from(products).forEach(product => {
+        const productName = product.querySelector("h4").innerText.toLowerCase();
+        if (productName.includes(input)) {
+            product.style.display = "block";
+        } else {
+            product.style.display = "none";
+        }
+    });
+}
+
+
